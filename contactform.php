a1b290af2c04856ae153a4a1f98b9cb2908140b4
thomas9900 <52283912+thomas9900@users.noreply.github.com>
2020-09-28 11:26:35 +0300

    p

--- contactform.php
+++ contactform.php
@@ -11,7 +11,7 @@ if (isset($_POST['submit'])) {
     $txt = "you hae received an e-mail from ".$name.".\n\n".$message;
 
     mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsend");
+    header("Location: index.html?mailsend");
 }
 
 ?>
